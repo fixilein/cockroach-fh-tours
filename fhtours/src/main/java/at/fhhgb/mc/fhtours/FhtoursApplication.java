ac1366d0cf46b91d4bffb8ff1a16deeafb202cd2
Emanuel K <emanuelkranjec@outlook.com>
2020-04-22 16:50:47 +0200

    project created; gitignore edited

---
+++ fhtours/src/main/java/at/fhhgb/mc/fhtours/FhtoursApplication.java
@@ -0,0 +1,13 @@
+package at.fhhgb.mc.fhtours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhtoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhtoursApplication.class, args);
+	}
+
+}
